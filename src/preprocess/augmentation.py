18fbe2248bb2d19681a5b058e62bef57a2f549cf
alexander <alr.popkov@gmail.com>
2021-08-23 22:07:48 +0300

    Updated augmenttion function

--- src/preprocess/augmentation.py
+++ src/preprocess/augmentation.py
@@ -1,65 +1,61 @@
 import albumentations as img_album
 
 
-def get_training_augmentation(image_size, normalize=None):
+def get_training_augmentation(image_size, weather=False, flips=True, extend_aug=False, normalize=None):
+    """
+    Training augmentation pipeline
+    Args:
+        image_size:  int, input image size
+        weather:     bool, use weather augmentation or not
+        flips:       bool, use horizontal and vertical flipping or not
+        extend_aug:  bool, use additional augmentation operations
+        normalize:   dict, config in config.py with image normalization params: mean, std
+
+    Returns: albumentations augmentation pipeline
+
+    """
     base_transform_list = [
-        img_album.IAAFliplr(p=0.5),
-        img_album.IAAFlipud(p=0.5),
         img_album.GaussianBlur(blur_limit=5, p=0.5),
         img_album.Rotate(limit=(10, 270)),
         img_album.MultiplicativeNoise(multiplier=(0.5, 1.2)),
         img_album.ChannelShuffle(p=0.5),
-        # img_album.Cutout(num_holes=20, max_h_size=5, max_w_size=5, always_apply=False, p=0.2)
     ]
-    weather_transform = [img_album.RandomSnow(p=0.3),
-                         img_album.RandomRain(p=0.3),
-                         img_album.RandomFog(p=0.2),
-                         img_album.RandomSunFlare(p=0.2)
-                         ]
-    train_transform = [
 
-        img_album.OneOf(base_transform_list, p=0.5),
-        img_album.OneOf(weather_transform, p=0.5),
+    if flips:
+        flip_transform = [img_album.OneOf([img_album.HorizontalFlip(p=0.5),
+                                           img_album.VerticalFlip(p=0.5)]
+                                          )
+                          ]
+        base_transform_list.extend(flip_transform)
 
-        img_album.OneOf([img_album.HorizontalFlip(p=0.5),
-                         img_album.VerticalFlip(p=0.5)]
-                        ),
+    if weather:
+        weather_transform = [img_album.RandomSnow(p=0.3),
+                             img_album.RandomRain(p=0.3),
+                             img_album.RandomFog(p=0.2),
+                             img_album.RandomSunFlare(p=0.2)
+                             ]
+        base_transform_list.extend(weather_transform)
 
-        img_album.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0),
+    train_transform = [img_album.OneOf(base_transform_list, p=0.5)]
 
-        img_album.PadIfNeeded(min_height=image_size, min_width=image_size, always_apply=True, border_mode=0),
-        # img_album.RandomCrop(height=image_size//10, width=image_size//10, p=0.4),
+    if extend_aug:
+        aug_extensions = [
+            img_album.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0),
+            img_album.IAAAdditiveGaussianNoise(p=0.2),
+            img_album.IAAPerspective(p=0.5),
+            img_album.CLAHE(p=0.5),
+            img_album.RandomBrightness(p=0.5),
+            img_album.RandomGamma(p=0.5),
+            img_album.IAASharpen(p=0.5),
+            img_album.Blur(blur_limit=3, p=0.5),
+            img_album.MotionBlur(blur_limit=3, p=0.5),
+            img_album.RandomContrast(p=0.5),
+            img_album.HueSaturationValue(p=0.5),
 
-        img_album.IAAAdditiveGaussianNoise(p=0.2),
-        img_album.IAAPerspective(p=0.5),
+        ]
+        train_transform.append(img_album.OneOf(aug_extensions, p=0.5))
 
-        img_album.OneOf(
-            [
-                img_album.CLAHE(p=0.5),
-                img_album.RandomBrightness(p=0.5),
-                img_album.RandomGamma(p=0.5),
-            ],
-            p=0.9,
-        ),
-
-        img_album.OneOf(
-            [
-                img_album.IAASharpen(p=0.5),
-                img_album.Blur(blur_limit=3, p=0.5),
-                img_album.MotionBlur(blur_limit=3, p=0.5),
-            ],
-            p=0.9,
-        ),
-
-        img_album.OneOf(
-            [
-                img_album.RandomContrast(p=0.5),
-                img_album.HueSaturationValue(p=0.5),
-            ],
-            p=0.9,
-        ),
-        img_album.Lambda(mask=round_clip_0_1)
-    ]
+    train_transform.append(img_album.Lambda(mask=round_clip_0_1))
 
     if normalize:
         # After normalization change array back to unit8 for further augmentation
